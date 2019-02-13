fdf0bacf14fa0bb4a8b7407e9e80106236e6fc2d
Mohammad Elhariry <mohammad.elhariry@aalto.fi>
2019-02-13 22:46:54 +0200

    Update score model to include game state Add iframe for html game in play template

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.postgres.fields import JSONField
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -62,5 +63,6 @@ class Transaction(models.Model):
 class Score(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     player = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    current_score = models.PositiveIntegerField(default=0)
+    current_score = models.FloatField(blank=True, null=True)
+    current_state = JSONField(blank=True, null=True)
     date = models.DateField(default=datetime.now, blank=True)
